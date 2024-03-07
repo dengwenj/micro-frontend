df36240929ca91f17455bd486a48149e3513b8da
朴睦 <927561237@qq.com>
2024-03-07 21:30:01 +0800

    micro-app 的实现步骤

--- 04_micro-app/main/src/index.tsx
+++ 04_micro-app/main/src/index.tsx
@@ -4,6 +4,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import microApp from '@micro-zoe/micro-app'
 
+/**
+ * 1. 对于我们的 micro-app 他里面的核心是创建一个 webcomponent 组件
+ * 2、获取 html，将模版放到 webComponent 中
+ * 3、css 做作用域隔离，js 做 proxy 沙箱
+ * 4、执行完毕后应用可以正常挂载
+ */
 microApp.start()
 
 const root = ReactDOM.createRoot(
